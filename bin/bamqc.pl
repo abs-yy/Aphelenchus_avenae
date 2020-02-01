94c84a8449e9625e15ea2ac3b5d86beb5156f17a
Yuki Yoshida <abs@sfc.keio.ac.jp>
2020-02-01 19:40:02 +0900

    Create bamqc.pl

---
+++ bin/bamqc.pl
@@ -0,0 +1,21 @@
+use G;
+use strict;
+
+my $ref = shift;
+my $read1 = shift;
+my $read2 = shift;
+my $label = shift;
+my $core = 64;
+die("four arguments necessary: ref, read1, read2, label") unless(length($label));
+
+unless(-e "$ref.bwt"){
+    system("bwa index $ref");
+}
+
+
+system("bwa mem $ref $read1 $read2 -t $core  > $label.mem.sam");
+system("samtools view -@ $core -bS $label.mem.sam > $label.mem.bam");
+system("samtools sort -@ $core $label.mem.bam $label.mem.sorted");
+system("samtools index $label.mem.sorted.bam");
+system("rm $label.mem.sam $label.mem.bam")
+system("samtools idxstat $label.mem.sorted.bam > $label.mem.sorted.bam.mapped")
